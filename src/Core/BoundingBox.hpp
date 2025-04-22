5155a5494cb906503e63b067b297cdddee707f4b
Grégor Sternat <gregor.sternat@epitech.eu>
2025-04-23 01:41:58 +0200

    `feat(core): add BoundingBox class for spatial management in raytracer`
    Added `BoundingBox` class to the Core module, supporting operations like intersection, union, containment checks, and access to bounds.

---
+++ src/Core/BoundingBox.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Math/Point.hpp"
+#include "Ray.hpp"
+
+namespace Raytracer::Core {
+
+class BoundingBox final {
+public:
+  constexpr BoundingBox() noexcept = default;
+  constexpr BoundingBox(const Math::Point<3> &min, const Math::Point<3> &max) noexcept;
+
+  [[nodiscard]] bool intersect(const Ray &ray) const noexcept;
+  [[nodiscard]] BoundingBox unite(const BoundingBox &other) const noexcept;
+  [[nodiscard]] constexpr bool contains(const Math::Point<3> &point) const noexcept;
+
+  [[nodiscard]] constexpr Math::Point<3> &getMin() const noexcept;
+  [[nodiscard]] constexpr Math::Point<3> &getMax() const noexcept;
+
+private:
+  Math::Point<3> m_min{};
+  Math::Point<3> m_max{};
+};
+} // namespace Raytracer::Core
