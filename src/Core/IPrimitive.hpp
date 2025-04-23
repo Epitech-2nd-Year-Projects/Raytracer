ec6f1662a24224f97aaf386e6037d2f89455a6f7
Grégor Sternat <gregor.sternat@epitech.eu>
2025-04-23 22:16:44 +0200

    feat(Core): extend IPrimitive interface with transformation, material, and state management
    Added methods for position, rotation, scale manipulation, and material handling in `IPrimitive` to support advanced object transformations and state access. Introduced corresponding private members for internal state storage.

--- src/Core/IPrimitive.hpp
+++ src/Core/IPrimitive.hpp
@@ -21,5 +21,29 @@ public:
   intersect(const Ray &ray) const noexcept = 0;
 
   [[nodiscard]] virtual BoundingBox getBoundingBox() const noexcept = 0;
+
+  virtual void transform(const Math::Point<3> &position,
+                         const Math::Vector<3> &rotation,
+                         const Math::Vector<3> &scale) noexcept = 0;
+
+  virtual void setPosition(const Math::Point<3> &position) noexcept = 0;
+  virtual void setRotation(const Math::Vector<3> &rotation) noexcept = 0;
+  virtual void setScale(const Math::Vector<3> &scale) noexcept = 0;
+  virtual void setMaterial(std::shared_ptr<IMaterial> material) noexcept = 0;
+
+  virtual [[nodiscard]] constexpr const Math::Point<3> &
+  getPosition() const noexcept = 0;
+  virtual [[nodiscard]] constexpr const Math::Vector<3> &
+  getRotation() const noexcept = 0;
+  virtual [[nodiscard]] constexpr const Math::Vector<3> &
+  getScale() const noexcept = 0;
+  virtual [[nodiscard]] const std::shared_ptr<IMaterial> &
+  getMaterial() const noexcept = 0;
+
+private:
+  Math::Point<3> m_position{};
+  Math::Vector<3> m_rotation{};
+  Math::Vector<3> m_scale{1.0, 1.0, 1.0};
+  std::shared_ptr<IMaterial> m_material{nullptr};
 };
 } // namespace Raytracer::Core
