a00ccec37b9f8472cb1a6f69b2e245451daa2a23
Grégor Sternat <gregor.sternat@epitech.eu>
2025-04-23 22:16:27 +0200

    feat(core): add `override` specifier to APrimitive methods
    Ensure derived methods in `APrimitive` properly override base class virtual functions by adding the `override` specifier. Improves code clarity and correctness in inheritance.

--- src/Core/APrimitive.hpp
+++ src/Core/APrimitive.hpp
@@ -14,28 +14,36 @@ public:
   APrimitive &operator=(const APrimitive &) = delete;
   APrimitive &operator=(APrimitive &&) = delete;
 
-  void setPosition(const Math::Point<3> &p) noexcept { m_position = p; }
-  void setRotation(const Math::Vector<3> &r) noexcept { m_rotation = r; }
-  void setScale(const Math::Vector<3> &s) noexcept { m_scale = s; }
+  void setPosition(const Math::Point<3> &p) noexcept override {
+    m_position = p;
+  }
+  void setRotation(const Math::Vector<3> &r) noexcept override {
+    m_rotation = r;
+  }
+  void setScale(const Math::Vector<3> &s) noexcept override { m_scale = s; }
   void setMaterial(std::unique_ptr<IMaterial> m) noexcept {
     m_material = std::move(m);
   }
 
-  [[nodiscard]] constexpr const Math::Point<3> &getPosition() const noexcept {
+  [[nodiscard]] constexpr const Math::Point<3> &
+  getPosition() const noexcept override {
     return m_position;
   }
-  [[nodiscard]] constexpr const Math::Vector<3> &getRotation() const noexcept {
+  [[nodiscard]] constexpr const Math::Vector<3> &
+  getRotation() const noexcept override {
     return m_rotation;
   }
-  [[nodiscard]] constexpr const Math::Vector<3> &getScale() const noexcept {
+  [[nodiscard]] constexpr const Math::Vector<3> &
+  getScale() const noexcept override {
     return m_scale;
   }
-  [[nodiscard]] const std::unique_ptr<IMaterial> &getMaterial() const noexcept {
+  [[nodiscard]] const std::unique_ptr<IMaterial> &
+  getMaterial() const noexcept override {
     return m_material;
   }
 
   void transform(const Math::Point<3> &p, const Math::Vector<3> &r,
-                 const Math::Vector<3> &s) noexcept {
+                 const Math::Vector<3> &s) noexcept override {
     setPosition(p);
     setRotation(r);
     setScale(s);
