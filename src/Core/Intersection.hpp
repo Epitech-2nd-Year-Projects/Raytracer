4ecd3411f6396a5fb53b8de852ced308880f6a19
Grégor Sternat <gregor.sternat@epitech.eu>
2025-04-24 13:50:55 +0200

    `refactor(core): replace shared_ptr with unique_ptr for IMaterial in Intersection`
    Refactored `Intersection` class to use `std::unique_ptr` instead of `std::shared_ptr` for `IMaterial`, ensuring clearer ownership semantics and improved memory management.

--- src/Core/Intersection.hpp
+++ src/Core/Intersection.hpp
@@ -12,12 +12,12 @@ class Intersection final {
 public:
   constexpr Intersection() noexcept = default;
   Intersection(const Math::Point<3> &point, const Math::Vector<3> &normal,
-               std::shared_ptr<IMaterial> material, double distance,
+               std::unique_ptr<IMaterial> material, double distance,
                bool isInside, const Math::Point<2> &uv) noexcept;
 
   [[nodiscard]] constexpr const Math::Point<3> &getPoint() const noexcept;
   [[nodiscard]] constexpr const Math::Vector<3> &getNormal() const noexcept;
-  [[nodiscard]] std::shared_ptr<IMaterial> getMaterial() const noexcept;
+  [[nodiscard]] std::unique_ptr<IMaterial> getMaterial() const noexcept;
   [[nodiscard]] constexpr double getDistance() const noexcept;
   [[nodiscard]] constexpr bool getIsInside() const noexcept;
   [[nodiscard]] constexpr const Math::Point<2> &getUv() const noexcept;
@@ -25,7 +25,7 @@ public:
 private:
   Math::Point<3> m_point{};
   Math::Vector<3> m_normal{};
-  std::shared_ptr<IMaterial> m_material{nullptr};
+  std::unique_ptr<IMaterial> m_material{nullptr};
   double m_distance{0.0};
   bool m_isInside{false};
   Math::Point<2> m_uv{0.0, 0.0};
