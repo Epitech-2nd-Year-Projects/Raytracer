34e42dba81a49619d2a97f0a13e9a7c72867fd0a
Grégor Sternat <gregor.sternat@epitech.eu>
2025-04-24 05:07:57 +0200

    `refactor(core): adjust includes and change material ownership to unique_ptr`
    Updated include paths for better modularity and switched material pointers from shared_ptr to unique_ptr to enforce unique ownership and improve memory management.

--- src/Core/IPrimitive.hpp
+++ src/Core/IPrimitive.hpp
@@ -3,12 +3,12 @@
 #include <memory>
 #include <optional>
 
-#include "BoundingBox.hpp"
-#include "IMaterial.hpp"
-#include "Intersection.hpp"
+#include "Core/BoundingBox.hpp"
+#include "Core/IMaterial.hpp"
+#include "Core/Intersection.hpp"
 #include "Math/Point.hpp"
 #include "Math/Vector.hpp"
-#include "Ray.hpp"
+#include "Core/Ray.hpp"
 
 namespace Raytracer::Core {
 
@@ -29,15 +29,15 @@ public:
   virtual void setPosition(const Math::Point<3> &position) noexcept = 0;
   virtual void setRotation(const Math::Vector<3> &rotation) noexcept = 0;
   virtual void setScale(const Math::Vector<3> &scale) noexcept = 0;
-  virtual void setMaterial(std::shared_ptr<IMaterial> material) noexcept = 0;
+  virtual void setMaterial(std::unique_ptr<IMaterial> material) noexcept = 0;
 
-  virtual [[nodiscard]] constexpr const Math::Point<3> &
+  [[nodiscard]] virtual const Math::Point<3> &
   getPosition() const noexcept = 0;
-  virtual [[nodiscard]] constexpr const Math::Vector<3> &
+  [[nodiscard]] virtual const Math::Vector<3> &
   getRotation() const noexcept = 0;
-  virtual [[nodiscard]] constexpr const Math::Vector<3> &
+  [[nodiscard]] virtual const Math::Vector<3> &
   getScale() const noexcept = 0;
-  virtual [[nodiscard]] const std::shared_ptr<IMaterial> &
+  [[nodiscard]] virtual const std::unique_ptr<IMaterial> &
   getMaterial() const noexcept = 0;
 };
 } // namespace Raytracer::Core
