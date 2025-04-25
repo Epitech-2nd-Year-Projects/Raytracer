70dd86a0ab985ec0f07a4a57db7ab15328a04ce1
Grégor Sternat <gregor.sternat@epitech.eu>
2025-04-25 20:43:06 +0200

    refactor(Color): inline methods and simplify implementation

--- src/Core/Color.hpp
+++ src/Core/Color.hpp
@@ -26,64 +26,74 @@ public:
    * @param green Green component [0, 255].
    * @param blue Blue component [0, 255].
    */
-  Color(double red, double green, double blue) noexcept;
+  Color(double red, double green, double blue) noexcept
+      : m_r(red), m_g(green), m_b(blue) {}
 
   /**
    * @brief Get red component.
    * @return Red value.
    */
-  [[nodiscard]] constexpr double getR() const noexcept;
+  [[nodiscard]] constexpr double getR() const noexcept { return m_r.get(); }
 
   /**
    * @brief Get green component.
    * @return Green value.
    */
-  [[nodiscard]] constexpr double getG() const noexcept;
+  [[nodiscard]] constexpr double getG() const noexcept { return m_g.get(); }
 
   /**
    * @brief Get blue component.
    * @return Blue value.
    */
-  [[nodiscard]] constexpr double getB() const noexcept;
+  [[nodiscard]] constexpr double getB() const noexcept { return m_b.get(); }
 
   /**
    * @brief Set red component.
    * @param red New red value.
    */
-  void setR(double red) noexcept;
+  void setR(double red) noexcept { m_r = red; }
 
   /**
    * @brief Set green component.
    * @param green New green value.
    */
-  void setG(double green) noexcept;
+  void setG(double green) noexcept { m_g = green; }
 
   /**
    * @brief Set blue component.
    * @param blue New blue value.
    */
-  void setB(double blue) noexcept;
+  void setB(double blue) noexcept { m_b = blue; }
 
   /**
    * @brief Component-wise multiplication.
    * @param other Other color.
    * @return Resulting color.
    */
-  [[nodiscard]] Color multiply(const Color &other) const noexcept;
+  [[nodiscard]] Color multiply(const Color &other) const noexcept {
+    return Color(getR() * other.getR() / 255.0, getG() * other.getG() / 255.0,
+                 getB() * other.getB() / 255.0);
+  }
 
   /**
    * @brief Component-wise addition.
    * @param other Other color.
    * @return Resulting color.
    */
-  [[nodiscard]] Color add(const Color &other) const noexcept;
+  [[nodiscard]] Color add(const Color &other) const noexcept {
+    return Color(getR() + other.getR(), getG() + other.getG(),
+                 getB() + other.getB());
+  }
 
   /**
    * @brief Component-wise scaling.
    * @param other Other color.
    * @return Resulting color.
    */
-  [[nodiscard]] Color scale(const Color &other) const noexcept;
+  [[nodiscard]] Color scale(const Color &other) const noexcept {
+    return Color(getR() * other.getR(), getG() * other.getG(),
+                 getB() * other.getB());
+  }
 
 private:
   Utility::Clamped<double, 0.0, 255.0> m_r{0.0};
