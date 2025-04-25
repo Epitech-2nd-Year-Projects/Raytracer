626a7e421841a8dcff90ddbd5cdab037dc362a35
Grégor Sternat <gregor.sternat@epitech.eu>
2025-04-25 19:14:02 +0200

    `feat(core): add Intersection class implementation`
    Provide core functionality for representing and accessing intersection details in the raytracing process, including point, normal, material, distance, and UV coordinates.

---
+++ src/Core/Intersection.cpp
@@ -0,0 +1,35 @@
+#include "Core/Intersection.hpp"
+
+
+namespace Raytracer::Core {
+Intersection::Intersection(const Math::Point<3> &point, const Math::Vector<3> &normal,
+             std::shared_ptr<IMaterial> material, double distance,
+             bool isInside, const Math::Point<2> &uv) noexcept
+  : m_point(point), m_normal(normal), m_material(std::move(material)),
+    m_distance(distance), m_isInside(isInside), m_uv(uv) {}
+
+
+const Math::Point<3>& Intersection::getPoint() const noexcept {
+  return m_point;
+}
+
+std::shared_ptr<IMaterial> Intersection::getMaterial() const noexcept {
+  return m_material;
+}
+
+double Intersection::getDistance() const noexcept {
+  return m_distance;
+}
+
+bool Intersection::getIsInside() const noexcept {
+  return m_isInside;
+}
+
+const Math::Vector<3>& Intersection::getNormal() const noexcept {
+  return m_normal;
+}
+
+const Math::Point<2>& Intersection::getUv() const noexcept {
+  return m_uv;
+}
+}
