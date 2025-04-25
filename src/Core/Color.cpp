22429f6d937e0df0884e1e1261692cf7e78b29ce
Grégor Sternat <gregor.sternat@epitech.eu>
2025-04-25 20:26:38 +0200

    `style(Color): simplify function formatting for improved readability`
    Refactored the formatting of functions in `Color.cpp` to a more concise, single-line style where applicable, enhancing code clarity and reducing unnecessary line breaks.

--- src/Core/Color.cpp
+++ src/Core/Color.cpp
@@ -3,48 +3,32 @@
 namespace Raytracer::Core {
 
 Color::Color(double red, double green, double blue) noexcept
-  : m_r(red), m_g(green), m_b(blue) {}
+    : m_r(red), m_g(green), m_b(blue) {}
 
-constexpr double Color::getB() const noexcept {
-  return m_b.get();
-}
+constexpr double Color::getB() const noexcept { return m_b.get(); }
 
-constexpr double Color::getG() const noexcept {
-  return m_g.get();
-}
+constexpr double Color::getG() const noexcept { return m_g.get(); }
 
-constexpr double Color::getR() const noexcept {
-  return m_r.get();
-}
+constexpr double Color::getR() const noexcept { return m_r.get(); }
 
-void Color::setB(double blue) noexcept {
-  m_b = blue;
-}
+void Color::setB(double blue) noexcept { m_b = blue; }
 
-void Color::setG(double green) noexcept {
-  m_g = green;
-}
+void Color::setG(double green) noexcept { m_g = green; }
 
-void Color::setR(double red) noexcept {
-  m_r = red;
-}
-
+void Color::setR(double red) noexcept { m_r = red; }
 
 Color Color::multiply(const Color &other) const noexcept {
-  return Color(getR() * other.getR() / 255.0,
-               getG() * other.getG() / 255.0,
+  return Color(getR() * other.getR() / 255.0, getG() * other.getG() / 255.0,
                getB() * other.getB() / 255.0);
 }
 
 Color Color::add(const Color &other) const noexcept {
-  return Color(getR() + other.getR(),
-               getG() + other.getG(),
+  return Color(getR() + other.getR(), getG() + other.getG(),
                getB() + other.getB());
 }
 
 Color Color::scale(const Color &other) const noexcept {
-  return Color(getR() * other.getR(),
-               getG() * other.getG(),
+  return Color(getR() * other.getR(), getG() * other.getG(),
                getB() * other.getB());
 }
-}
+} // namespace Raytracer::Core
