0bfa0f618bbdcc6a51cf90964287af9d55119c54
Grégor Sternat <gregor.sternat@epitech.eu>
2025-04-25 20:27:08 +0200

    style(core): improve code formatting and consistency in Intersection.cpp
    Enhanced code readability by adjusting indentation and spacing, improving alignment, and ensuring consistent formatting for function definitions.

--- src/Core/Intersection.cpp
+++ src/Core/Intersection.cpp
@@ -1,15 +1,14 @@
 #include "Core/Intersection.hpp"
 
-
 namespace Raytracer::Core {
-Intersection::Intersection(const Math::Point<3> &point, const Math::Vector<3> &normal,
-             std::shared_ptr<IMaterial> material, double distance,
-             bool isInside, const Math::Point<2> &uv) noexcept
-  : m_point(point), m_normal(normal), m_material(std::move(material)),
-    m_distance(distance), m_isInside(isInside), m_uv(uv) {}
+Intersection::Intersection(const Math::Point<3> &point,
+                           const Math::Vector<3> &normal,
+                           std::shared_ptr<IMaterial> material, double distance,
+                           bool isInside, const Math::Point<2> &uv) noexcept
+    : m_point(point), m_normal(normal), m_material(std::move(material)),
+      m_distance(distance), m_isInside(isInside), m_uv(uv) {}
 
-
-const Math::Point<3>& Intersection::getPoint() const noexcept {
+const Math::Point<3> &Intersection::getPoint() const noexcept {
   return m_point;
 }
 
@@ -17,19 +16,13 @@ std::shared_ptr<IMaterial> Intersection::getMaterial() const noexcept {
   return m_material;
 }
 
-double Intersection::getDistance() const noexcept {
-  return m_distance;
-}
+double Intersection::getDistance() const noexcept { return m_distance; }
 
-bool Intersection::getIsInside() const noexcept {
-  return m_isInside;
-}
+bool Intersection::getIsInside() const noexcept { return m_isInside; }
 
-const Math::Vector<3>& Intersection::getNormal() const noexcept {
+const Math::Vector<3> &Intersection::getNormal() const noexcept {
   return m_normal;
 }
 
-const Math::Point<2>& Intersection::getUv() const noexcept {
-  return m_uv;
-}
-}
+const Math::Point<2> &Intersection::getUv() const noexcept { return m_uv; }
+} // namespace Raytracer::Core
