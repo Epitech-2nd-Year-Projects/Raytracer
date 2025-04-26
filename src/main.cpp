5d2915beb4b72639e6639f9c57155a683e5d5991
Yanis Kernoua <yanis.kernoua@epitech.eu>
2025-04-26 18:23:30 +0200

    refactor(main): restructure main function and enhance usage instructions

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,41 @@
+/**
+ * @file Main.cpp
+ * @brief Main entry point for the raytracer application.
+ */
+
 #include <iostream>
+#include <string_view>
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
-  std::cout << "Hello, World!" << std::endl;
+/**
+ * @brief Prints the usage instructions for the raytracer application.
+ * @param programName The name of the program.
+ */
+void printUsage(const std::string_view programName) {
+  std::cout << "USAGE: " << programName << " <SCENE_FILE>\n"
+            << "\tSCENE_FILE: scene configuration\n";
+}
 
+/**
+ * @brief Main function for the raytracer application.
+ * @param argc Number of command-line arguments.
+ * @param argv Array of command-line arguments.
+ * @return Exit status code.
+ */
+int main(int argc, char *argv[]) {
+  const std::string_view programName = argv[0];
+
+  if (argc == 2 && (std::string_view(argv[1]) == "-help")) {
+    printUsage(programName);
+    return 0;
+  }
+
+  if (argc != 2) {
+    printUsage(programName);
+    return 84;
+  }
+
+  const std::string_view sceneFile = argv[1];
+
+  // call the parser, build the scene, and render it
   return 0;
 }
