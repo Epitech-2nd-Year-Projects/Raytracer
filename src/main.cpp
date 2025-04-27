56ae362acf530d68c56e816e38565d557bde0d19
Yanis Kernoua <yanis.kernoua@epitech.eu>
2025-04-27 14:55:04 +0200

    feat(main): update usage instructions and implement basic debug mode handling

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,11 @@
  * @brief Main entry point for the raytracer application.
  */
 
+#include "Core/Renderer.hpp"
+#include "Parser/SceneParser.hpp"
 #include <iostream>
+#include <memory>
+#include <optional>
 #include <string_view>
 
 /**
@@ -11,8 +15,9 @@
  * @param programName The name of the program.
  */
 void printUsage(const std::string_view programName) {
-  std::cout << "USAGE: " << programName << " <SCENE_FILE>\n"
-            << "\tSCENE_FILE: scene configuration\n";
+  std::cout << "USAGE: " << programName << " <SCENE_FILE> [-d]\n"
+            << "\tSCENE_FILE: scene configuration\n"
+            << "\t-d: enable debug mode\n";
 }
 
 /**
@@ -34,8 +39,26 @@ int main(int argc, char *argv[]) {
     return 84;
   }
 
+  if (argc == 3 && std::string_view(argv[2]) == "-d") {
+    // Impelement a Logger class
+  }
+
   const std::string_view sceneFile = argv[1];
 
-  // call the parser, build the scene, and render it
+  try {
+    std::optional<std::unique_ptr<Raytracer::Core::Scene>> scene =
+        Raytracer::Parser::SceneParser().parseFile(sceneFile.data());
+
+    if (!scene) {
+      return 84;
+    }
+
+    Raytracer::Core::Renderer renderer(1920, 1080);
+    renderer.render(*scene.value(), "output.ppm");
+  } catch (const std::exception &e) {
+    std::cerr << "Error: " << e.what() << "\n";
+    return 84;
+  }
+
   return 0;
 }
