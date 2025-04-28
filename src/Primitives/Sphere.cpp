b1dbeed28f0a9c438e18d08e5ee4c0f16a289a14
Grégor Sternat <gregor.sternat@epitech.eu>
2025-04-28 04:30:18 +0200

    feat(sphere): integrate transformation support in intersection logic

--- src/Primitives/Sphere.cpp
+++ src/Primitives/Sphere.cpp
@@ -21,10 +21,11 @@ const Math::Point<3> &Sphere::getCenter() const noexcept { return m_center; }
 
 [[nodiscard]] std::optional<Core::Intersection>
 Sphere::intersect(const Core::Ray &ray) const noexcept {
-  Math::Vector<3> oc = ray.getOrigin() - m_center;
+  Core::Ray localRay = getTransform().inverseTransformRay(ray);
+  Math::Vector<3> oc = localRay.getOrigin() - m_center;
 
-  double a = ray.getDirection().dot(ray.getDirection());
-  double b = 2.0 * oc.dot(ray.getDirection());
+  double a = localRay.getDirection().dot(localRay.getDirection());
+  double b = 2.0 * oc.dot(localRay.getDirection());
   double c = oc.dot(oc) - m_radius * m_radius;
 
   double delta = b * b - 4 * a * c;
@@ -34,8 +35,10 @@ Sphere::intersect(const Core::Ray &ray) const noexcept {
   double t1 = (-b - std::sqrt(delta)) / (2.0 * a);
   double t2 = (-b + std::sqrt(delta)) / (2.0 * a);
 
-  bool t1Valid = t1 >= ray.getMinDistance() && t1 <= ray.getMaxDistance();
-  bool t2Valid = t2 >= ray.getMinDistance() && t2 <= ray.getMaxDistance();
+  bool t1Valid =
+      t1 >= localRay.getMinDistance() && t1 <= localRay.getMaxDistance();
+  bool t2Valid =
+      t2 >= localRay.getMinDistance() && t2 <= localRay.getMaxDistance();
   if (!t1Valid && !t2Valid) {
     return std::nullopt;
   }
@@ -49,24 +52,31 @@ Sphere::intersect(const Core::Ray &ray) const noexcept {
     t = t2;
   }
 
-  Math::Point<3> intersectionPoint = ray.at(t);
-  Math::Vector<3> dirToCenter = Math::Vector<3>(intersectionPoint - m_center);
-  Math::Vector<3> normal = dirToCenter / m_radius;
+  Math::Point<3> localIntersectionPoint = localRay.at(t);
+  Math::Vector<3> localDirToCenter =
+      Math::Vector<3>(localIntersectionPoint - m_center);
+  Math::Vector<3> localNormal = localDirToCenter / m_radius;
 
   bool isInside = false;
-  if (normal.dot(ray.getDirection()) > 0) {
-    normal = normal * -1.0;
+  if (localNormal.dot(localRay.getDirection()) > 0) {
+    localNormal = localNormal * -1.0;
     isInside = true;
   }
 
-  double phi = std::atan2(normal.m_components[2], normal.m_components[0]);
-  double theta = std::acos(normal.m_components[1]);
-
+  double phi =
+      std::atan2(localNormal.m_components[2], localNormal.m_components[0]);
+  double theta = std::acos(localNormal.m_components[1]);
   double u = (phi + M_PI) / (2 * M_PI);
   double v = theta / M_PI;
 
-  return Core::Intersection(intersectionPoint, normal, this->getMaterial(), t,
-                            isInside, Math::Point<2>{u, v});
+  Math::Point<3> worldIntersectionPoint =
+      getTransform().transformPoint(localIntersectionPoint);
+  Math::Vector<3> worldNormal = getTransform().transformNormal(localNormal);
+  double worldDistance = (worldIntersectionPoint - ray.getOrigin()).length();
+
+  return Core::Intersection(worldIntersectionPoint, worldNormal,
+                            this->getMaterial(), worldDistance, isInside,
+                            Math::Point<2>{u, v});
 }
 
 [[nodiscard]] Core::BoundingBox Sphere::getBoundingBox() const noexcept {
