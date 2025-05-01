10288f877a34f1fcb8669052793a13de06bb8175
LixiosDelios <alexandre.guillaud@epitech.eu>
2025-05-01 10:46:24 +0200

    feat(Cylinder) : add Cylinder class for primitive operations

--- src/Factory/PrimitiveFactory.hpp
+++ src/Factory/PrimitiveFactory.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "Primitives/Cylinder.hpp"
 #include "Primitives/Plane.hpp"
 #include "Primitives/Sphere.hpp"
 namespace Raytracer::Factory {
@@ -24,6 +25,15 @@ public:
   [[nodiscard]] static std::unique_ptr<Core::IPrimitive>
   createPlane(const std::string &axis, const Math::Point<3> &position) {
     return std::make_unique<Primitives::Plane>(axis, position);
+  }
+
+  /**
+   * @brief Create a cylinder primitive
+   */
+  [[nodiscard]] static std::unique_ptr<Core::IPrimitive>
+  createCylinder(const std::string &axis, const Math::Point<3> &position,
+                 const double radius, const double height) {
+    return std::make_unique<Primitives::Cylinder>(axis, position, radius, height);
   }
 };
 } // namespace Raytracer::Factory
